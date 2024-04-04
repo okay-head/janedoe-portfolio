29444ec66f61f3bedfa08e95b29c236fb592c351
okay-head <68184623+okay-head@users.noreply.github.com>
2024-04-04 19:58:44 +0530

    responsiveness fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,33 +5,17 @@ export default function Home() {
   return (
     <main>
       <div
-        className={`container-custom relative m-2 flex min-h-[calc(100vh-1rem)] flex-col justify-evenly border-2 border-black p-10 ${`bg-teal-base-200`}`}
+        className={`container-custom relative m-4 flex min-h-screen flex-col justify-evenly p-5 lg:min-h-[calc(100vh-5rem)] lg:p-9 ${`bg-teal-base-200`}`}
       >
         <button className='nav-toggle absolute right-5 top-5'>
           <img src={hamburger} alt='Open Menu' />
         </button>
 
-        <div className='adjust-top-spacing h-14'></div>
-        <div className='grid grid-cols-2 gap-4'>
-          <section className='hero-text relative grid min-h-[75vh] place-items-center'>
-            <span className='decorators text-text-lighter absolute left-0 top-0 inline-block'></span>
-            <span className='decorators text-text-lighter absolute bottom-0 right-0 inline-block'></span>
-
-            <div className='flex flex-col gap-4'>
-              <h1 className='font-urbanist text-3xl font-semibold uppercase'>
-                Hi, I'm
-              </h1>
-              <h1 className='font-urbanist text-8xl font-semibold uppercase'>
-                john doe
-              </h1>
-              <h2 className='text-xl'>
-                A veteran software developer based in Los angeles
-              </h2>
-            </div>
-          </section>
-          <section className='hero-img'>
-            <div className='hero-img-bg bg-teal-base-400 relative h-full max-w-[40rem] overflow-hidden'>
-              <div className='relative left-1/2 top-1/2 aspect-square max-h-64 -translate-x-1/2 -translate-y-1/2'>
+        <div className='adjust-top-spacing h-4 lg:h-14'></div>
+        <div className='grid grid-rows-2 lg:min-h-[75vh] lg:grid-cols-2 lg:grid-rows-1  lg:gap-4'>
+          <section className='hero-img min-h-[40vh] lg:order-2'>
+            <div className='hero-img-bg bg-teal-base-400 relative mx-auto grid h-full max-w-[40rem] place-content-center overflow-hidden'>
+              <div className='relative aspect-square h-40 lg:h-64'>
                 <div className='hero-img-placeholder relative z-10 h-full w-full bg-slate-600'>
                   {/* hero image here */}
                 </div>
@@ -44,14 +28,31 @@ export default function Home() {
                 <img
                   src={zigzag}
                   alt='zigzag'
-                  className='absolute -bottom-[18%] -left-1/4 aspect-square h-8'
+                  className='absolute -bottom-[18%] -left-1/4 hidden aspect-square h-8 lg:inline-block'
                 />
                 <img
                   src={zigzag}
                   alt='zigzag'
-                  className='absolute -bottom-[36%] -right-[30%] aspect-square h-8'
+                  className='absolute -bottom-[36%] -right-[30%] hidden aspect-square h-8 lg:inline-block'
                 />
               </div>
+            </div>
+          </section>
+
+          <section className='hero-text relative grid place-items-center lg:order-1'>
+            <span className='decorators text-text-lighter absolute left-0 top-0 inline-block'></span>
+            <span className='decorators text-text-lighter absolute bottom-0 right-0 inline-block'></span>
+
+            <div className='flex flex-col gap-4'>
+              <h1 className='font-urbanist text-2xl font-semibold uppercase lg:text-3xl'>
+                Hi, I'm
+              </h1>
+              <h1 className='font-urbanist text-6xl font-semibold uppercase lg:text-8xl'>
+                john doe
+              </h1>
+              <h2 className='text-lg lg:text-xl'>
+                A veteran software developer based in Los angeles
+              </h2>
             </div>
           </section>
         </div>
