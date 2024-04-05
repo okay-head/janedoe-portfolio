f84f16efb93bd3436388530ca08c4c27106edfa4
okay-head <68184623+okay-head@users.noreply.github.com>
2024-04-05 06:39:26 +0530

    feat: hero section works

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import hamburger from './../assets/hamburger-bars.svg'
+import download from './../assets/download.svg'
 import slantBars from './../assets/slant-bars.svg'
 import zigzag from './../assets/zigzag.svg'
 export default function Home() {
@@ -14,7 +15,7 @@ export default function Home() {
         <div className='adjust-top-spacing h-4 lg:h-14'></div>
         <div className='grid grid-rows-2 lg:min-h-[75vh] lg:grid-cols-2 lg:grid-rows-1  lg:gap-4'>
           <section className='hero-img min-h-[40vh] lg:order-2'>
-            <div className='hero-img-bg bg-teal-base-400 relative mx-auto grid h-full max-w-[40rem] place-content-center overflow-hidden'>
+            <div className='hero-img-bg relative mx-auto grid h-full max-w-[40rem] place-content-center overflow-hidden bg-teal-base-400'>
               <div className='relative aspect-square h-40 lg:h-64'>
                 <div className='hero-img-placeholder relative z-10 h-full w-full bg-slate-600'>
                   {/* hero image here */}
@@ -40,10 +41,10 @@ export default function Home() {
           </section>
 
           <section className='hero-text relative grid place-items-center lg:order-1'>
-            <span className='decorators text-text-lighter absolute left-0 top-0 inline-block'></span>
-            <span className='decorators text-text-lighter absolute bottom-0 right-0 inline-block'></span>
+            <span className='decorators absolute left-0 top-0 inline-block text-text-lighter'></span>
+            <span className='decorators absolute bottom-0 right-0 inline-block text-text-lighter'></span>
 
-            <div className='flex flex-col gap-4'>
+            <div className='flex flex-col gap-6'>
               <h1 className='font-urbanist text-2xl font-semibold uppercase lg:text-3xl'>
                 Hi, I'm
               </h1>
@@ -51,13 +52,36 @@ export default function Home() {
                 john doe
               </h1>
               <h2 className='text-lg lg:text-xl'>
-                A veteran software developer based in Los angeles
+                I develop 3D visuals, user interfaces and web applications.
               </h2>
             </div>
           </section>
         </div>
-        <div className='cta-group'>
-          <button className='border border-black px-3 py-1'>Resume</button>
+        <div className='cta-group relative -top-9 flex gap-8 text-lg sm:mx-auto lg:ms-24'>
+          <div className='btn-wrapper group relative inline-block'>
+            <span className='btn-ouline absolute left-3 top-3 z-0 h-full w-full -translate-x-3 -translate-y-3 border border-black bg-slate-600 transition-all group-hover:-translate-y-0 group-hover:translate-x-0'></span>
+            <button
+              id='resume'
+              className='relative z-10 border border-black bg-teal-base-200 px-6 py-1.5'
+            >
+              Resume
+              <img
+                src={download}
+                alt='download icon'
+                className='ms-1.5 inline-block w-[1em]'
+              />
+            </button>
+          </div>
+
+          <div className='btn-wrapper group relative inline-block'>
+            <span className='btn-ouline absolute left-3 top-3 z-0 h-full w-full -translate-x-3 -translate-y-3 border border-black bg-slate-600 transition-all group-hover:-translate-y-0 group-hover:translate-x-0'></span>
+            <button
+              id='hireme'
+              className='relative z-10 border border-black bg-teal-base-200 px-8 py-1.5'
+            >
+              Hire me
+            </button>
+          </div>
         </div>
       </div>
     </main>
