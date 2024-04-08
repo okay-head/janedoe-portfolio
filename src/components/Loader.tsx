1770b7d79c78df3d387ba0d8bd087b739dff9b8e
okay-head <68184623+okay-head@users.noreply.github.com>
2024-04-08 22:01:07 +0530

    loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,61 @@
+import { useAnimate } from 'framer-motion'
+
 export default function Loader() {
+  const [scope, animate] = useAnimate()
+
+  async function handleAnimation() {
+    // -- box size up --
+    await animate(
+      '#loader',
+      {
+        opacity: [0, 1],
+        width: [0, '10vw'],
+        height: [0, '10vw'],
+      },
+      {
+        // type: 'spring',
+        ease: 'easeInOut',
+        // stiffness: 300,
+        // damping: 50,
+        duration: 2,
+      },
+    )
+
+    // back shadow
+    await animate('.btn-ouline', { y: [0, 30], x: [0, 50] }, { duration: 1 })
+    await animate(
+      '#wrapper',
+      { backgroundColor: 'black' },
+      { duration: 1, ease: 'easeInOut' },
+    )
+    await animate(
+      '#loader',
+      { backgroundColor: 'transparent', borderColor: 'white' },
+      { duration: 1 },
+    )
+    animate(
+      '.btn-ouline',
+      { backgroundColor: 'transparent', borderColor: 'white' },
+      { duration: 1 },
+    )
+  }
+  handleAnimation()
+
   return (
-    <>
-      <h1 className='absolute inset-0 bg-white text-4xl'>Loader</h1>
-    </>
+    <div
+      ref={scope}
+      className='loader absolute inset-0 grid place-content-center'
+    >
+      <div id='wrapper' className='wrapper absolute inset-0 bg-white'></div>
+      {/* <h1 className='absolute inset-0 bg-white text-4xl'>Loader</h1> */}
+      <div className='loader-wrapper group relative inline-block max-w-max'>
+        <button
+          id='loader'
+          // className='relative z-10 h-72 w-72 border border-black bg-white'
+          className='relative z-10 border-2 border-black bg-white'
+        ></button>
+        <span className='btn-ouline absolute left-0 top-0 z-0 h-full w-full border-2 border-black bg-black'></span>
+      </div>
+    </div>
   )
 }
