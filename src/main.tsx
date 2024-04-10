00b7b20072d8eebae9590fcc279ef1eeba7e98f5
okay-head <68184623+okay-head@users.noreply.github.com>
2024-04-10 08:53:33 +0530

    perf: add motionconfig for reduced motion

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+import { MotionConfig } from 'framer-motion'
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <MotionConfig reducedMotion='user'>
+    <App />
+  </MotionConfig>,
+)
